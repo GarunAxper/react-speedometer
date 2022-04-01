d06b1be9b367f6bb19f2779da94cdd7de249245c
Vazgen Khachatryan <vazgen.khachatryan@topsoft.am>
2022-04-01 18:51:49 +0400

    added socket server

---
+++ back-app/Speedometer/Program.cs
@@ -0,0 +1,13 @@
+﻿using Speedometer;
+using WebSocketSharp.Server;
+
+const string socketUrl = "ws://localhost:12345";
+const string socketPath = "/Velocity";
+
+var webSocketServer = new WebSocketServer(socketUrl);
+
+webSocketServer.AddWebSocketService<VelocityBehavior>(socketPath);
+webSocketServer.Start();
+
+Console.ReadKey(true);
+webSocketServer.Stop();
---
+++ back-app/Speedometer/VelocityBehavior.cs
@@ -0,0 +1,30 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Speedometer;
+
+public class VelocityBehavior : WebSocketBehavior
+{
+    private static readonly string FilePath =
+        Path.Combine(Environment.CurrentDirectory , "velocity-data.txt");
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        var data = GetVelocityData();
+
+        while (true)
+        {
+            foreach (var v in data)
+            {
+                Thread.Sleep(51);
+                Send(v);
+            }
+        }
+    }
+
+    private static string[] GetVelocityData()
+    {
+        var rawData = File.ReadAllText(FilePath);
+        return rawData.Split(", ");
+    }
+}
